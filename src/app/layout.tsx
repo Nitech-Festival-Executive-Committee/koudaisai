1fa2aa097933ca6bc60348bbf98433d05d6dacd2
senntou <wataru_00117@icloud.com>
2024-09-21 22:47:57 +0900

    metadata viewport の変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Footer from "@/components/Footer";
 // copyright: "Copyright (C) 2024 名古屋工業大学第62回工大祭実行委員会 All Rights Reserved.",
 export const metadata: Metadata = {
   title: "第62回 工大祭 2024",
-  viewport: "width=device-width, initial-scale=1",
   description:
     "名古屋工業大学の大学祭「工大祭」の公式HP! 第62回工大祭「Ignition」は2024年11月16日(土)・17日(日)に開催予定!! 是非お越しください!!",
   keywords: "工大祭,第62回工大祭,名工祭,名古屋工業大学,名工大",
@@ -25,6 +24,8 @@ export const metadata: Metadata = {
 
 export const viewport = {
   themeColor: "#f7791e",
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
