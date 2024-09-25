a88126f66b27e3f5d9b20c84ff73bf95cc43091e
ViniciusBrJp <vgccarmona@gmail.com>
2024-09-26 01:20:46 +0900

    preFestival PR後の修正

--- src/components/PreviousFestival/PreviousFestival.tsx
+++ src/components/PreviousFestival/PreviousFestival.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./PreviousFestival.module.scss";
-import TemplatePrevFestival from "./TemplatePrevFestival";
+import PrevFestivalCard from "./PrevFestivalCard";
 
 export default function PreviousFestival() {
   return (
@@ -13,49 +13,49 @@ export default function PreviousFestival() {
       </h4>
       {/*TODO 下のdivにfadeDownTriggerをあとで追加*/}
       <div>
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/61st"
           thisImage="./img/61st.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/60th"
           thisImage="./img/60th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/59th"
           thisImage="./img/59th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/58th"
           thisImage="./img/58th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/57th"
           thisImage="./img/57th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/56th"
           thisImage="./img/56th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/55th"
           thisImage="./img/55th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/54th"
           thisImage="./img/54th.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/53rd"
-          thisImage="./img/53th.webp"
+          thisImage="./img/53rd.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/52nd"
-          thisImage="./img/52th.webp"
+          thisImage="./img/52nd.webp"
         />
-        <TemplatePrevFestival
+        <PrevFestivalCard
           thisHref="https://www.koudaisai.com/51st"
-          thisImage="./img/51th.webp"
+          thisImage="./img/51st.webp"
         />
       </div>
     </div>
