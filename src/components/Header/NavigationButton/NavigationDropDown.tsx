02882b3903de9cbf519a95b4818e14766424d909
senntou <wataru_00117@icloud.com>
2024-09-27 21:41:38 +0900

    企画ボタンホバー時のアニメーションをちょうせい

--- src/components/Header/NavigationButton/NavigationDropDown.tsx
+++ src/components/Header/NavigationButton/NavigationDropDown.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import styles from "./NavigationItem.module.scss";
+import { useState } from "react";
 
 interface NavigationDropDownProps {
   text: string;
@@ -7,24 +9,51 @@ interface NavigationDropDownProps {
     url: string;
   }[];
 }
+
+const dropdownMenuVariants = {
+  hidden: {
+    opacity: 0,
+    height: 0,
+  },
+  visible: {
+    opacity: 1,
+    height: "auto",
+  },
+};
+
 export default function NavigationDropDown(props: NavigationDropDownProps) {
+  const [dropdownMenuIsVisible, setDropdownMenuIsVisible] = useState(false);
   return (
-    <div className={`${styles.navItem} nav-item pl-4 pl-md-0`}>
+    <motion.div
+      className={`${styles.navItem} nav-item pl-4 pl-md-0`}
+      // hover時、クリック時にドロップダウンを表示する
+      onHoverStart={() => setDropdownMenuIsVisible(true)}
+      onClick={() => setDropdownMenuIsVisible(!dropdownMenuIsVisible)}
+      onHoverEnd={() => setDropdownMenuIsVisible(false)}
+    >
       <a className={`${styles.navLink} nav-link`} aria-label={props.text}>
         {props.text}
       </a>
-      <div className={`${styles.dropdownMenu}`}>
-        {props.items.map((item) => (
-          <a
-            key={item.url}
-            className={`${styles.dropdownItem} dropdown-item`}
-            href={item.url}
-            aria-label={item.text}
-          >
-            {item.text}
-          </a>
-        ))}
-      </div>
-    </div>
+      <motion.div
+        className={styles.dropdownMenu}
+        initial="hidden"
+        variants={dropdownMenuVariants}
+        animate={dropdownMenuIsVisible ? "visible" : "hidden"}
+        transition={{ duration: 0.5 }}
+      >
+        <div className={styles.dropdownItemContainer}>
+          {props.items.map((item) => (
+            <a
+              key={item.url}
+              className={`${styles.dropdownItem} dropdown-item`}
+              href={item.url}
+              aria-label={item.text}
+            >
+              {item.text}
+            </a>
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
   );
 }
