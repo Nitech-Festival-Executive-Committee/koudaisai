070a1e431341e26055144ccf0b80e0f6d3984e0a
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-02 01:19:10 +0900

    highlight-underlineをコンポーネント化

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -3,6 +3,7 @@ import ContentBox from "@/components/Content/ContentBox/ContentBox";
 import ContentTitle from "@/components/Content/ContentTitle/ContentTitle";
 import PageWrapper from "@/components/Content/PageWrapper/PageWrapper";
 import SectionBody from "@/components/Content/SectionBody/SectionBody";
+import Underline from "@/components/Content/Underline/Underline";
 
 export default function Faq() {
   return (
@@ -11,13 +12,32 @@ export default function Faq() {
       <PageWrapper>
         <SectionBody>
           <ContentBox title={"第62回工大祭の開催日時は?"}>
-            <p>第62回工大祭「Ignition」は<span className="highlight-underline">2024年11月16日(土)・17日(日) 10:00-18:00</span>に開催予定!!</p>
+            <p>
+              第62回工大祭「Ignition」は
+              <Underline>2024年11月16日(土)・17日(日) 10:00-18:00</Underline>
+              に開催予定!!
+            </p>
           </ContentBox>
           <ContentBox title={"来場予約や整理券は必要ですか?"}>
-            <p><span className="highlight-underline">来場予約は不要です!</span>是非ご来場ください!<br/>また、<span className="highlight-underline">一部企画は整理券が必要です。</span><br/>詳しくは<a href="../project" aria-label="企画一覧を開く">企画一覧</a>をご覧ください。</p>
+            <p>
+              <Underline>来場予約は不要です!</Underline>
+              是非ご来場ください!
+              <br />
+              また、
+              <Underline>一部企画は整理券が必要です。</Underline>
+              <br />
+              詳しくは
+              <a href="../project" aria-label="企画一覧を開く">
+                企画一覧
+              </a>
+              をご覧ください。
+            </p>
           </ContentBox>
           <ContentBox title={"駐輪場はありますか?"}>
-            <p>2号館裏, 52号館前にあります。必ず指定場所に駐輪するようにお願いします。</p>
+            <p>
+              2号館裏,
+              52号館前にあります。必ず指定場所に駐輪するようにお願いします。
+            </p>
           </ContentBox>
           <ContentBox title={"どのエリアを回ることができますか?"}>
             <p>準備中</p>
@@ -28,13 +48,29 @@ export default function Faq() {
             <p>第62回工大祭のテーマは「Ignition」!</p>
           </ContentBox>
           <ContentBox title={"企画一覧/タイムテーブル"}>
-            <p> <a href="../project" aria-label="企画一覧を開く">企画一覧</a> / <a href="../timetable" aria-label="タイムテーブルを開く">タイムテーブル</a></p>
+            <p>
+              {" "}
+              <a href="../project" aria-label="企画一覧を開く">
+                企画一覧
+              </a>{" "}
+              /{" "}
+              <a href="../timetable" aria-label="タイムテーブルを開く">
+                タイムテーブル
+              </a>
+            </p>
           </ContentBox>
           <ContentBox title={"駐車場はありますか?"}>
-            <p>学内に来訪者用の駐車場はございません。近くの有料駐車場、もしくは公共交通機関をご利用ください。公共交通機関は<a href="../access">アクセス</a>をご覧ください。</p>
+            <p>
+              学内に来訪者用の駐車場はございません。近くの有料駐車場、もしくは公共交通機関をご利用ください。公共交通機関は
+              <a href="../access">アクセス</a>をご覧ください。
+            </p>
           </ContentBox>
           <ContentBox title={"飲食できる場所はありますか?"}>
-            <p>模擬店で購入したものは<span className="highlight-underline">外で食べ歩き可能</span>です。</p>
+            <p>
+              模擬店で購入したものは
+              <Underline>外で食べ歩き可能</Underline>
+              です。
+            </p>
           </ContentBox>
           <ContentBox title={"ゴミ箱はどこにありますか?"}>
             <p>2号館西, 3号館南, 52号館南にあります。</p>
@@ -49,28 +85,38 @@ export default function Faq() {
             <p>利用可能です。</p>
           </ContentBox>
         </SectionBody>
-        </PageWrapper>
+      </PageWrapper>
 
-        <ContentTitle title="お問い合わせ" size={1} bigTitle={true} />
-        <PageWrapper>
+      <ContentTitle title="お問い合わせ" size={1} bigTitle={true} />
+      <PageWrapper>
         <SectionBody>
           <ContentTitle title="お問合せ先" size={2} />
           <ContentBox title={""}>
-            <p>お問い合わせの前に、このページに同じ内容が無いか確認の後、お問い合わせいただくようよろしくお願いします。
-              <br/>工大祭についてのお問い合わせは以下の連絡先にて承っております。
-              <br/>工大祭当日のメールでのお問い合わせは返信ができないこともございますのでご了承ください。</p>
-              <ContentBox title={"メールアドレス"}>
-                <p><a href="mailto:info@koudaisai.com">info@koudaisai.com</a></p>
-          </ContentBox>
+            <p>
+              お問い合わせの前に、このページに同じ内容が無いか確認の後、お問い合わせいただくようよろしくお願いします。
+              <br />
+              工大祭についてのお問い合わせは以下の連絡先にて承っております。
+              <br />
+              工大祭当日のメールでのお問い合わせは返信ができないこともございますのでご了承ください。
+            </p>
+            <ContentBox title={"メールアドレス"}>
+              <p>
+                <a href="mailto:info@koudaisai.com">info@koudaisai.com</a>
+              </p>
+            </ContentBox>
           </ContentBox>
         </SectionBody>
 
         <SectionBody>
           <ContentTitle title="郵便" size={2} />
           <ContentBox title={"住所"}>
-            <p>〒466-8555
-              <br/>愛知県名古屋市昭和区御器所町大学会館地下一階
-              <br/>第62回名古屋工業大学工大祭実行委員会</p>
+            <p>
+              〒466-8555
+              <br />
+              愛知県名古屋市昭和区御器所町大学会館地下一階
+              <br />
+              第62回名古屋工業大学工大祭実行委員会
+            </p>
           </ContentBox>
         </SectionBody>
       </PageWrapper>
