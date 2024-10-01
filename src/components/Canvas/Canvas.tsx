3cd2306366f2c5cc5c3aedf04a727bac690856a1
ViniciusBrJp <vgccarmona@gmail.com>
2024-10-01 16:44:32 +0900

    Canvas background and stating logo

---
+++ src/components/Canvas/Canvas.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Canvas.module.scss";
+import Image from "next/image";
+
+export default function Canvas() {
+  return (
+    <div className={styles.canvas}>
+      <Image
+        className={styles.canvasBackground}
+        src="/img/canvas.webp"
+        alt="CanvasBackground"
+        width={100}
+        height={100}
+      />
+      <Image
+        className={styles.canvasImg}
+        src="/img/62ndstartImg.webp"
+        alt="CanvasImg"
+        layout="intrinsic"
+        width={300}
+        height={100}
+      />
+    </div>
+  );
+}
