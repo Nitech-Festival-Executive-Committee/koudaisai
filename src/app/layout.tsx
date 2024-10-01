85dd06cfda0dcd786f12fc4b1225f06c11f0966d
senntou <wataru_00117@icloud.com>
2024-10-01 21:58:28 +0900

    bootstrap廃止

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "@/styles/style.css";
 
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
 import SnsIconList from "@/components/Sns/SnsIconList";
 
 // TODO: authorとcopyrightを作成
