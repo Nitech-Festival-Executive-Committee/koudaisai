c67f6ebf43d627c7fadfc3090d8982d268b9713e
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-04 13:01:47 +0900

    projectDataを取り込んでcontentBoxを生成する機能を実装

---
+++ src/app/project/projectInterface.ts
@@ -0,0 +1,62 @@
+interface contentBox {
+  title: string;
+  content: string | HTMLElement | (string | HTMLElement)[];
+}
+
+// 期待するフィード
+export interface baseProjectData {
+  link: contentBox; // project以下のリンク(ex. seiyu)
+  name: contentBox; // プロジェクト名
+  description: contentBox; // 企画紹介
+  day1?: contentBox; // 開催時刻。開催しない場合は空欄
+  day2?: contentBox; // 開催時刻。開催しない場合は空欄
+  place: contentBox; // 開催場所
+  contact: contentBox; // お問合せ先
+  [key: string]: contentBox | undefined; // 任意のフィード
+}
+
+export const createProjectData = (data: {
+  link: string;
+  name: string;
+  description: string | HTMLElement;
+  day1?: string;
+  day2?: string;
+  place: string | HTMLElement;
+  contact: string | HTMLElement;
+  [key: string]: string | contentBox | HTMLElement | undefined;
+}): baseProjectData => {
+  return {
+    link: { title: "リンク", content: data.link },
+    name: { title: "プロジェクト名", content: data.name },
+    description: { title: "企画紹介", content: data.description },
+    day1: data.day1 ? { title: "1日目", content: data.day1 } : undefined,
+    day2: data.day2 ? { title: "2日目", content: data.day2 } : undefined,
+    place: { title: "開催場所", content: data.place },
+    contact: { title: "お問合せ先", content: data.contact },
+    // 任意のフィールドにも同様に title を設定
+    ...Object.keys(data).reduce(
+      (acc, key) => {
+        if (
+          ![
+            "link",
+            "name",
+            "description",
+            "day1",
+            "day2",
+            "place",
+            "contact",
+          ].includes(key)
+        ) {
+          const customField = data[key] as contentBox;
+          acc[key] = {
+            title: customField.title,
+            content: customField.content,
+            isHidden: customField.isHidden ?? false, // isHiddenが設定されていない場合はfalseを設定
+          };
+        }
+        return acc;
+      },
+      {} as Record<string, contentBox | undefined>
+    ),
+  };
+};
