dfd72a344f3374b747d13c8fe64d6fca11dd40e7
ViniciusBrJp <vgccarmona@gmail.com>
2024-10-07 15:19:07 +0900

    MediaQuery: Rewrite

---
+++ src/components/Recommendation/Recommendation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ContentTitle from "../Content/ContentTitle/ContentTitle";
+
+export default function Recommendation() {
+  return (
+    <div>
+      <ContentTitle title="おすすめ" size={2} bigTitle />
+    </div>
+  );
+}
---
+++ src/components/Recommendation/RecommendationCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ContentBox from "../Content/ContentBox/ContentBox";
+
+export default function RecommendationCard() {
+  return;
+  <ContentBox title={" "}></ContentBox>;
+}
