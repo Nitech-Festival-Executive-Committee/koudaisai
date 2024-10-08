14b06fd4d5bc8fae8d76b9cd43ccbe22b98e94b4
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-08 15:20:45 +0900

    [WIP]webp以外の画像を自動でwebpに変換して表示

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import ImageMinimizerPlugin from "image-minimizer-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,6 +10,22 @@ const nextConfig = {
   },
   sassOptions: {
     includePaths: ["./src/styles"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer.push(
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [["imagemin-webp", { quality: 75 }]],
+            },
+          },
+        })
+      );
+    }
+
+    return config;
   },
 };
 
