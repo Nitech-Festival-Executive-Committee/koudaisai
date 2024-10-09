7e56d74d0584e736c9a91fc6c9d24d5d2b043a15
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-09 10:42:26 +0900

    よく使う項目を追加

--- src/app/project/projectInterface.ts
+++ src/app/project/projectInterface.ts
@@ -1,6 +1,13 @@
 import { ReactNode, isValidElement, cloneElement } from "react";
 
+// 事前に用意された項目と任意の項目のどちらも、下の要素が全て設定されていることを保証する
 export interface projectBox {
+  title: string;
+  content: string | ReactNode | (string | ReactNode)[];
+  isHidden: boolean;
+}
+
+export interface optionalProjectBox {
   title: string;
   content: string | ReactNode | (string | ReactNode)[];
   isHidden?: boolean;
@@ -10,46 +17,68 @@ export interface projectBox {
 export interface baseProjectData {
   link: projectBox; // project以下のリンク(ex. seiyu)
   name: projectBox; // プロジェクト名
+  day1: projectBox | undefined; // 開催時刻。開催しない場合は空欄
+  day2: projectBox | undefined; // 開催時刻。開催しない場合は空欄
+  projectTag: projectBox | undefined; // 企画タグ(追加分)
   description: projectBox; // 企画紹介
-  day1?: projectBox; // 開催時刻。開催しない場合は空欄
-  day2?: projectBox; // 開催時刻。開催しない場合は空欄
   place: projectBox; // 開催場所
+  capacity: projectBox | undefined; // 定員
+  precautions: projectBox; // 注意事項
+  prize: projectBox | undefined; // 景品
   contact: projectBox; // お問合せ先
-  projectTag?: projectBox; // 企画タグ
   [key: string]: projectBox | undefined; // 任意のフィード
 }
 
 export const createProjectData = (data: {
   link: string;
   name: string;
-  description: string | ReactNode;
   day1?: string | ReactNode;
   day2?: string | ReactNode;
-  place: string | ReactNode;
-  contact: string | ReactNode;
   projectTag?: (string | ReactNode)[];
-  [key: string]: string | projectBox | ReactNode | undefined;
+  description: string | ReactNode;
+  place: string | ReactNode;
+  capacity?: string | ReactNode;
+  precautions?: string | ReactNode | (string | ReactNode)[];
+  prize?: ReactNode;
+  contact: string | ReactNode;
+  [key: string]: string | optionalProjectBox | ReactNode | undefined;
 }): baseProjectData => {
   return {
-    // 必須フィードを設定
     link: { title: "リンク", content: data.link, isHidden: true },
     name: { title: "プロジェクト名", content: data.name, isHidden: true },
-    description: {
-      title: "企画紹介",
-      content: data.description,
-      isHidden: false,
-    },
     day1: data.day1
       ? { title: "1日目", content: data.day1, isHidden: false }
       : undefined,
     day2: data.day2
       ? { title: "2日目", content: data.day2, isHidden: false }
       : undefined,
-    place: { title: "開催場所", content: data.place, isHidden: false },
-    contact: { title: "お問合せ先", content: data.contact, isHidden: false },
     projectTag: data.projectTag
       ? { title: "企画タグ", content: data.projectTag, isHidden: true }
       : undefined,
+    description: {
+      title: "企画紹介",
+      content: data.description,
+      isHidden: false,
+    },
+    place: { title: "開催場所", content: data.place, isHidden: false },
+    capacity: data.capacity
+      ? { title: "定員", content: data.capacity, isHidden: false }
+      : undefined,
+    precautions: {
+      title: "注意事項",
+      content: data.precautions
+        ? Array.isArray(data.precautions)
+          ? data.precautions.map((item, index) =>
+              isValidElement(item) ? cloneElement(item, { key: index }) : item
+            ) // 自動でkeyを付与
+          : data.precautions
+        : undefined,
+      isHidden: false,
+    },
+    prize: data.prize
+      ? { title: "景品", content: data.prize, isHidden: false }
+      : undefined,
+    contact: { title: "お問合せ先", content: data.contact, isHidden: false },
     // 任意のフィールドにも同様に設定
     ...Object.keys(data).reduce(
       (acc, key) => {
@@ -60,9 +89,12 @@ export const createProjectData = (data: {
             "description",
             "day1",
             "day2",
-            "place",
-            "contact",
             "projectTag",
+            "place",
+            "capacity",
+            "precautions",
+            "prize",
+            "contact",
           ].includes(key)
         ) {
           const customField = data[key] as projectBox;
@@ -84,3 +116,5 @@ export const createProjectData = (data: {
     ),
   };
 };
+
+// TODO: returnする時、引数の順番を変えないようにしたい
