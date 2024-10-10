2bb91a46d7f09d42ce7734fe372a9a1a2d9a7b1c
ViniciusBrJp <vgccarmona@gmail.com>
2024-10-11 01:04:31 +0900

    MediaQUery delete unnecessary files

--- src/components/Recommendation/RecommendationCard.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ContentBox from "../Content/ContentBox/ContentBox";
-
-export default function RecommendationCard() {
-  return;
-  <ContentBox title={" "}></ContentBox>;
-}
