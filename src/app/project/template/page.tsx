735ee747f2b5d6c8ce061f4909771fb3fba69a98
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-15 16:59:24 +0900

    企画ページのmetadataをprojectDataから生成するように変更

--- src/app/project/template/page.tsx
+++ src/app/project/template/page.tsx
@@ -9,8 +9,8 @@ import { timeScheduleDay1, timeScheduleDay2 } from "./templateSchedule";
 import { NITFES_EDITION, NITFES_YEAR } from "@/const/const";
 
 export const metadata = {
-  title: `企画名 - 第${NITFES_EDITION}回工大祭`, // TODO: 企画名をprojectDataから取得する
-  description: `${NITFES_YEAR}年 第${NITFES_EDITION}回工大祭開催「企画名」の詳細ページです`, // TODO: 企画名をprojectDataから取得する
+  title: `${templateProjectData.name} - 第${NITFES_EDITION}回工大祭`,
+  description: `${NITFES_YEAR}年 第${NITFES_EDITION}回工大祭開催「${templateProjectData.name}」の詳細ページです`,
 };
 
 export default function ProjectTemplate() {
