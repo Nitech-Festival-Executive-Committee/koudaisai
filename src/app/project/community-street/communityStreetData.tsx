614e030b4c9ef51199e103a9a858fbdfba2787e1
senntou <wataru_00117@icloud.com>
2024-10-17 10:22:09 +0900

    コミュニティーストリートページを追加

---
+++ src/app/project/community-street/communityStreetData.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/jsx-key */
+import { ProjectData } from "@/types/projectInterface";
+import { CATEGORY } from "@/types/types";
+import { convertScheduleToReactNode } from "@/utils/converter";
+import { createDate } from "@/utils/date";
+
+const schedule = {
+  day1: {
+    startDate: createDate(1, 10, 0),
+    endDate: createDate(1, 16, 0),
+  },
+  day2: {
+    startDate: createDate(2, 10, 0),
+    endDate: createDate(2, 15, 30),
+  },
+};
+
+const place = "23号館前通路";
+
+// TODO: `public/62nd/project/community-street/brochure.webp`を配置
+// TODO: `public/62nd/project/community-street/logo.webp`を配置
+// TODO: swiper-slideにスライドを追加
+// TODO: 必要があればタグを追加
+export const communityStreetData: ProjectData = {
+  // ContentBoxに含まないデータ
+  link: "community-street",
+  name: "コミュニティーストリート",
+  tags: [],
+  category: [CATEGORY.EXHIBITION],
+  summary: "各テント内に設営されているブース出展を楽しんでください！",
+
+  // ProjectBoxに変換する必要があるデータ
+  schedule,
+  place,
+
+  projectBoxList: [
+    {
+      title: "企画紹介",
+      content: (
+        <p>
+          各テント内に設営されているブース出展を楽しんでください！ <br />
+          各ブースに設置されたスタンプをすべて集めると景品と交換できます！
+        </p>
+      ),
+    },
+    {
+      title: "開催日程",
+      content: convertScheduleToReactNode(schedule),
+    },
+    {
+      title: "開催場所",
+      content: place,
+    },
+    {
+      title: "お問い合わせ先",
+      content: "地域局 三浦碧斗 aoto.miura@koudaisai.com",
+    },
+  ],
+};
