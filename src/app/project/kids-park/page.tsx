1e5d14c5cafd43e0b84d91693802b6024c107cb7
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-18 17:16:39 +0900

    キッズパークページ作成

---
+++ src/app/project/kids-park/page.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-key */
+import React from "react";
+import Project from "@/components/Project/Project/Project";
+import { NITFES_EDITION, NITFES_YEAR } from "@/const/const";
+import { kidsPark } from "./kidsPark";
+
+export const metadata = {
+  title: `${kidsPark.name} - 第${NITFES_EDITION}回工大祭`,
+  description: `${NITFES_YEAR}年 第${NITFES_EDITION}回工大祭開催「${kidsPark.name}」の詳細ページです`,
+};
+
+// 関数名を変更(ex. MiniGame)
+export default function KidsPark() {
+  return <Project projectData={kidsPark}></Project>;
+}
