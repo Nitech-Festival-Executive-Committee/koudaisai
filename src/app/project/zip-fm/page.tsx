bd601423885f0426b376dd5907b2bc95d7e430ff
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-18 18:25:52 +0900

    ZIP-FMページ作成

---
+++ src/app/project/zip-fm/page.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Project from "@/components/Project/Project/Project";
+import { zipFM as projectData } from "./zipFM";
+import { NITFES_EDITION, NITFES_YEAR } from "@/const/const";
+import PageWrapper from "@/components/Content/PageWrapper/PageWrapper";
+import ContentTitle from "@/components/Content/ContentTitle/ContentTitle";
+import ContentBox from "@/components/Content/ContentBox/ContentBox";
+import ContentImage from "@/components/Content/ContentImage/ContentImage";
+import SectionBody from "@/components/Content/SectionBody/SectionBody";
+
+export const metadata = {
+  title: `${projectData.name} - 第${NITFES_EDITION}回工大祭`,
+  description: `${NITFES_YEAR}年 第${NITFES_EDITION}回工大祭開催「${projectData.name}」の詳細ページです`,
+};
+
+// 関数名を変更(ex. MiniGame)
+export default function ZipFM() {
+  return (
+    <Project projectData={projectData} logoPath={""} brochurePath={""}>
+      <ContentTitle title={"ナビゲーター紹介"} size={2} bigTitle />
+      <PageWrapper>
+        <SectionBody>
+          <ContentBox title={"Hillary"}>
+            <ContentImage
+              img={"/62nd/project/zip-fm/Hillary.webp"}
+              alt={"Hillary"}
+            />
+            <ul>
+              <li>⽣年⽉⽇: 7⽉22⽇</li>
+              <li>出⾝: 愛知県 瀬戸市</li>
+              <li>
+                趣味: サバイバルゲーム、ロードバイク、
+                映画鑑賞、スイーツ作り、弾き語り、歌を歌うこと、カメラ
+              </li>
+              <li>特技: ネイル</li>
+              <li>
+                経歴:
+                アメリカ・ニューヨーク生まれ、日本育ちの現役大学生。インターナショナルスクールに通っていたが、小学5年生の時に「日本の中学校に通いたい!」という想いから日本語を勉強し始める。2017年からラジオサンキューにて月曜夕方パーソナリティーとして活動を開始する。2018年から「ハイスクールテリア」というJK番組のメインパーソナリティーを担当。チャレンジ精神旺盛、現役大学生ナビゲーター!
+              </li>
+            </ul>
+          </ContentBox>
+        </SectionBody>
+        <SectionBody>
+          <ContentBox title={"まるり"}>
+            <ContentImage
+              img={"/62nd/project/zip-fm/Maruri.webp"}
+              alt={"まるり"}
+            />
+            <ul>
+              <li>⽣年⽉⽇: 3⽉4⽇</li>
+              <li>出⾝: 福岡県 春日市</li>
+              <li>趣味: カメラ、FEELCYCLE</li>
+              <li>特技: モノマネ</li>
+              <li>
+                経歴:
+                幼少期から高校3年生まで合唱団に所属。声楽を学びながら音楽と隣り合わせの生活を送り、数々のステージに出演。2018
+                年 4
+                月より男女デュオ「まるりとりゅうが」として活動。2022年4月からはソロ・アーティストとして始動し、5月に優里作詞・作曲の
+                1st Digital
+                Single「好きだよ」でソロデビュー。抜群の歌唱力はもちろん、動画やインスタライブで見せる、飾らず親しみやすいキャラクターと愛らしいルックスで、同世代の男女からの絶大な支持を集めている。2022年8/31(水)に配信リリースした「ホントの私」で、ビクターエンタテインメントからメジャーデビュー。
+              </li>
+            </ul>
+          </ContentBox>
+        </SectionBody>
+        <SectionBody>
+          <ContentBox title={"中川大輔"}>
+            <ContentImage
+              img={"/62nd/project/zip-fm/Nakagawa.webp"}
+              alt={"中川大輔"}
+            />
+            <ul>
+              <li>⽣年⽉⽇: 4⽉5⽇</li>
+              <li>出⾝: 愛知県名古屋市</li>
+              <li>
+                趣味:
+                ハンバーガー食べ歩き・アウトドア&BBQ・旅先でのショットグラス収集
+              </li>
+              <li>特技： 剣道二段・バスケットボール・英会話(TOEIC 920点)</li>
+              <li>
+                経歴:
+                南山大学経営学部卒業大学在学中にアメリカ・アリゾナ州立大学へ留学、心と身体のサイズをアメリカナイズされる海外留学中、勉強しながら聞いたラジオをきっかけに、
+                ラジオの「リスナーとの距離の近さ・親しみやすさ」に惹かれる帰国後はZIP-FMナビゲータースクールに通い、ラジオDJ/MC業を目指すテレビ局でADとして勤務後、コミュニティラジオでDJキャリアをスタートZIP-FMでADを経て、2017年4月にミュージック・ナビゲーターデビュー!目標は「たくさん人の心を動かせるナビゲーター」
+              </li>
+            </ul>
+          </ContentBox>
+        </SectionBody>
+      </PageWrapper>
+    </Project>
+  );
+}
---
+++ src/app/project/zip-fm/zipFM.tsx
@@ -0,0 +1,147 @@
+import Underline from "@/components/Content/Underline/Underline";
+import { ProjectData } from "@/types/projectInterface";
+import { CATEGORY } from "@/types/types";
+import { createDate } from "@/utils/date";
+
+const schedule = {
+  day2: {
+    startDate: createDate({
+      date: 2,
+      hour: 14,
+      minute: 30,
+    }),
+    endDate: createDate({
+      date: 2,
+      hour: 16,
+      minute: 0,
+    }),
+  },
+};
+
+const place = "2号館前ステージ";
+
+// 変数名を変更(ex. miniGame)
+// ファイル名も変更(ex. miniGame.tsx)
+export const zipFM: ProjectData = {
+  // ContentBoxに含まないデータ
+  link: "zip-fm", // 複数単語の場合はハイフンつなぎで記述
+  name: "ZIP-FM",
+  tags: [],
+  category: [
+    CATEGORY.STAGE_PROJECT,
+    CATEGORY.FAMILY,
+    CATEGORY.STUDENT,
+    CATEGORY.EXPERIENCE,
+  ],
+  summary:
+    "特設ステージで大人気ラジオ局ZIP-FMと工大祭の内容盛りだくさんのコラボステージ!ナビゲーターのHillaryとまるり、そしてMCとして中川大輔が登場し、来場者参加型の企画や、GENZの公開録音を実施します",
+
+  // ProjectBoxに変換する必要があるデータ
+  schedule,
+  place,
+
+  projectBoxList: [
+    {
+      title: "企画紹介",
+      content: (
+        <p>
+          <Underline>ステージ企画</Underline>
+          <br />
+          特設ステージで大人気ラジオ局ZIP-FMと内容盛りだくさんのコラボステージの開催します!
+          <br />
+          <Underline>
+            ステージ企画はHillaryチーム VS まるりチームの対決型
+          </Underline>
+          で、そこに来訪者が各ナビゲーターのチームに参加する形となります!
+          <br />
+          行う企画は、「以心伝心ゲーム」「50:50アンケート」や「〇×クイズ」「Beef
+          or Chicken」の4つです!ZIP-FMのナビゲーターの方と共に対決しよう!
+          <br />
+          また、企画に参加していただいた方は、もれなく、豪華な参加賞や景品がもらえます!
+          <br />
+          <Underline>
+            なんと、1万円分のプリペイドカードが当たるゲームもあるとか…!
+          </Underline>
+          <br />
+          みなさんのご来場、お待ちしています!
+          <br />
+          <br />
+          <Underline>スピーカー企画</Underline>
+          <br />
+          スピーカーから音楽が流れるよ!ぜひ聴いてみてね
+        </p>
+      ),
+    },
+    {
+      title: "",
+      content: (
+        <p>
+          <Underline>ステージ企画</Underline>
+          <br />
+          <Underline>
+            ZIP-FM×工大祭 SPECIAL TALK LIVE supported by アドバンス電気工業
+          </Underline>
+          <br />
+          <br />
+          <Underline>スピーカー企画</Underline>
+          <br />
+          52号館南・53号館西・19号館西・4号館前に設置されているスピーカーからラジオや音楽が流れます!
+          <br />
+          また、聞きたい流したい曲がある方は、「#ZIP×工大祭」をX(旧Twitter)でつけてポストしてください!
+        </p>
+      ),
+    },
+    {
+      title: "開催日程",
+      content: (
+        <p>
+          <Underline>ステージ企画</Underline>
+          <br />
+          2日目(11/17(日)): 14:30～16:00
+          <br />
+          <br />
+          <Underline>スピーカー企画</Underline>
+          <br />
+          1日目(11/16(土)): 10:00～16:30
+          <br />
+          2日目(11/17(日)): 10:00～16:00
+        </p>
+      ),
+    },
+    {
+      title: "開催場所",
+      content: (
+        <p>
+          <Underline>ステージ企画</Underline>
+          <br />
+          2号館前ステージ
+          <br />
+          <br />
+          <Underline>スピーカー企画</Underline>
+          <br />
+          52号館南・53号館西・19号館西・4号館前
+        </p>
+      ),
+    },
+    {
+      title: "参加ナビゲーター",
+      content: (
+        <ul>
+          <li>中川大輔さん</li>
+          <li>Hillaryさん</li>
+          <li>まるりさん</li>
+        </ul>
+      ),
+    },
+    {
+      title: "注意事項",
+      content: (
+        <p>
+          <Underline>
+            録音・録画・写真撮影などは本企画でのいかなる形での記録撮影は工大祭実行委員会が撮影しているのを除き、禁止いたします。
+          </Underline>
+        </p>
+      ),
+    },
+  ],
+};
