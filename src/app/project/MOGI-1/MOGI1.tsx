9b0079a117a766a3aaa6dd1350ab8aabc53e4587
ViniciusBrJp <vgccarmona@gmail.com>
2024-10-18 22:25:15 +0900

    MOGI-1 写真以外

---
+++ src/app/project/MOGI-1/MOGI1.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/jsx-key */
+import { ProjectData } from "@/types/projectInterface";
+import { CATEGORY } from "@/types/types";
+import { convertScheduleToReactNode } from "@/utils/converter";
+import { createDate } from "@/utils/date";
+
+// TODO: タグ確認
+// TODO:企画紹介文の!!確認
+
+const schedule = {
+  day1: {
+    startDate: createDate({
+      date: 1,
+      hour: 10,
+      minute: 30,
+    }),
+    endDate: createDate({
+      date: 1,
+      hour: 15,
+      minute: 30,
+    }),
+  },
+  day2: {
+    startDate: createDate({
+      date: 2,
+      hour: 10,
+      minute: 30,
+    }),
+    endDate: createDate({
+      date: 2,
+      hour: 15,
+      minute: 0,
+    }),
+  },
+};
+
+const place = "投票所:3号館南ゴミステーションテント 表彰：2号館前ステージ";
+
+// 変数名を変更(ex. miniGame)
+// ファイル名も変更(ex. miniGame.tsx)
+export const mogi1: ProjectData = {
+  // ContentBoxに含まないデータ
+  link: "mogi1", // 複数単語の場合はハイフンつなぎで記述
+  name: "MOGI-1 グランプリ",
+  tags: [],
+  category: [CATEGORY.STUDENT, CATEGORY.EXHIBITION],
+  summary: "模擬店の人気投票を行う。",
+
+  // ProjectBoxに変換する必要があるデータ
+  schedule,
+  place,
+
+  projectBoxList: [
+    {
+      title: "企画紹介",
+      content:
+        " 工大祭来訪者に最も良かった模擬店とメニューを投票してもらい、模擬店部門では1位から3位、メニュー部門では1位を後夜祭で表彰します！",
+    },
+    {
+      title: "開催日程",
+      content: convertScheduleToReactNode(schedule),
+    },
+    {
+      title: "開催場所",
+      content: (
+        <p>
+          投票所:3号館南ゴミステーションテント
+          <br />
+          表彰：2号館前ステージ
+        </p>
+      ),
+    },
+    {
+      title: "ルール説明",
+      content:
+        "投票方法：3号館南ゴミステーションテントの投票所で模擬店部門の投票シールとメニュー部門の投票用紙を配っています。",
+    },
+    {
+      title: "お問い合わせ先",
+      content: "模擬局　中野貴博　takahiro.nakano@koudaisai.com",
+    },
+    {
+      title: "企画特有の内容",
+      content:
+        "3号館南ごみステーションテントの投票所で模擬店部門の投票シールとメニュー部門の投票用紙を配っている。",
+    },
+  ],
+};
