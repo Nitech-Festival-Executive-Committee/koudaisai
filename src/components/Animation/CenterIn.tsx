b10cfc9c11fafa161d1b8f9873a0c06ea152b669
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-24 15:51:42 +0900

    Framer Motionでアニメーションコンポーネントを作成

---
+++ src/components/Animation/CenterIn.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { motion, useInView } from "framer-motion";
+import { ReactNode, useRef } from "react";
+
+interface CenterIn {
+  children: ReactNode;
+  duration?: number;
+  delay?: number;
+}
+
+export default function CenterIn({
+  children,
+  duration = 0.5,
+  delay = 0,
+}: CenterIn) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref, {
+    once: true,
+    margin: "-100px 0px 0px 0px", // 画面に入ってから100px超えたら表示
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isInView ? 1 : 0 }}
+      transition={{ duration, ease: "easeOut", delay }}
+    >
+      {children}
+    </motion.div>
+  );
+}
