ec4dca3302834fa4f79442da4cd0fe1ed6d98113
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-03 22:30:22 +0900

    [WIP]企画ページのBento UIを作成

--- src/components/Project/BentoUI/BentoUI.tsx
+++ src/components/Project/BentoUI/BentoUI.tsx
@@ -6,59 +6,57 @@ import {
   IconSignature,
   IconTableColumn,
 } from "@tabler/icons-react";
+import { seiyu } from "@/app/project/seiyu/seiyu";
+import { artfulCampus } from "@/app/project/artful-campus/artfulCampus";
 
 export function BentoUI() {
   return (
     <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-      {items.map((item, i) => (
+      {items.map((item, index) => (
         <BentoGridItem
-          style={{
-            background: "#ebebeb",
-            borderRadius: "10px",
-            boxShadow: "8px 6px 8px 0px #dadada",
-          }}
-          key={i}
+          key={index}
+          className={item.className}
           title={item.title}
           description={item.description}
-          header={item.header}
-          className={item.className}
           icon={item.icon}
+          style={{
+            borderRadius: "10px",
+            boxShadow: "8px 6px 8px 0px #dadada",
+            background: `url(${item.img}) no-repeat center / cover`,
+          }}
         />
       ))}
     </BentoGrid>
   );
 }
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-);
 const items = [
   {
-    title: "The Dawn of Innovation",
+    title: seiyu.name,
     description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
     className: "md:col-span-2",
     icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    img: `/62nd/project/${seiyu.link}/brochure.webp`,
   },
   {
     title: "The Digital Revolution",
     description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
     className: "md:col-span-1",
     icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    img: `/62nd/project/${artfulCampus.link}/brochure.webp`,
   },
   {
     title: "The Art of Design",
     description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
     className: "md:col-span-1",
     icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    img: `/62nd/project/${seiyu.link}/brochure.webp`,
   },
   {
     title: "The Power of Communication",
     description:
       "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
     className: "md:col-span-2",
     icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    img: `/62nd/project/${seiyu.link}/brochure.webp`,
   },
 ];
--- src/lib/bento-grid.tsx
+++ src/lib/bento-grid.tsx
@@ -23,26 +23,23 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
   icon,
   style,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
   icon?: React.ReactNode;
   style?: React.CSSProperties;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent flex space-y-4",
         className
       )}
-      style={style}
+      style={{ ...style, flexDirection: "column", justifyContent: "flex-end" }}
     >
-      {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
