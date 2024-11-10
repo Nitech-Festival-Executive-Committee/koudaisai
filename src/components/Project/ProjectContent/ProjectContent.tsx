94e40f5aa2caf910d01571150fdc0253dc273199
senntou <wataru_00117@icloud.com>
2024-11-11 01:19:13 +0900

    企画ページのコンテンツに左右フェードインアニメーション追加

--- src/components/Project/ProjectContent/ProjectContent.tsx
+++ src/components/Project/ProjectContent/ProjectContent.tsx
@@ -1,5 +1,6 @@
 import ContentBox from "@/components/Content/ContentBox/ContentBox";
 import { ProjectBox } from "@/types/projectInterface";
+import Animation from "@/components/Animation/Animation";
 
 interface ProjectContentProps {
   projectBoxList: ProjectBox[];
@@ -14,13 +15,15 @@ export default function ProjectContent({
         // contentが文字列またはReactNodeの場合
         if (data?.content) {
           return (
-            <ContentBox key={index} title={data.title}>
-              {typeof data.content === "string" ? (
-                <p>{data.content}</p>
-              ) : (
-                data.content
-              )}
-            </ContentBox>
+            <Animation key={index} animationType={data.position || "right"}>
+              <ContentBox title={data.title}>
+                {typeof data.content === "string" ? (
+                  <p>{data.content}</p>
+                ) : (
+                  data.content
+                )}
+              </ContentBox>
+            </Animation>
           );
         }
 
