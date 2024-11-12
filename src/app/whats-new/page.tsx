cab4b95434c25293b4180076ee2f95539ed57481
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-12 19:53:52 +0900

    お知らせページを追加

--- src/app/whats-new/page.tsx
+++ src/app/whats-new/page.tsx
@@ -4,11 +4,8 @@ import PageWrapper from "@/components/Content/PageWrapper/PageWrapper";
 import SectionBody from "@/components/Content/SectionBody/SectionBody";
 import Animation from "@/components/Animation/Animation";
 import { NITFES_EDITION, NITFES_YEAR } from "@/const/const";
-import Button from "@mui/joy/Button";
-import Card from "@mui/joy/Card";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
 import styles from "./page.module.scss";
+import WhatsNewCard from "@/components/WhatsNewCard/WhatsNewCard";
 
 export const metadata = {
   title: `What's new? - 第${NITFES_EDITION}回工大祭`,
@@ -37,19 +34,6 @@ export default function Faq() {
             }
           />
           <WhatsNewCard
-            title="企画カードの改善"
-            text="企画の表示を変更し、去年よりも見やすく・おしゃれになりました!"
-            img={"/62nd/whats-new/projectCard.webp"}
-          />
-          <WhatsNewCard
-            title="おすすめ機能の最適化"
-            text="開催時間やカテゴリ分けに基づいて、より関連した企画がおすすめに表示されるようになりました"
-            // TODO: おすすめ機能が実装されたら画像を追加
-          />
-        </SectionBody>
-
-        <SectionBody>
-          <WhatsNewCard
             title="3Dマップの作成"
             text="当日の企画開催場所をより視覚的に紹介します"
             // 公開後に表示
@@ -57,17 +41,39 @@ export default function Faq() {
             // link={"https://shika174.github.io/Nitech_3D-MAP/"}
           />
           <WhatsNewCard
-            title="企画ページのレイアウト変更"
-            text="企画ページのレイアウトを改善し、PC・スマホのどちらでもより見やすく、探している情報に到達しやすくなりました。"
+            title="お知らせの表示変更"
+            text="お知らせ表示がコンパクトになり、より見やすくなりました!"
+            img="/62nd/whats-new/announcement.webp"
+            linkText="トップページに移動"
+            link={"../"}
+          />
+          <WhatsNewCard
+            title="企画カードの改善"
+            text="企画の表示を変更し、去年よりも見やすく・おしゃれになりました!"
+            img={"/62nd/whats-new/projectCard.webp"}
             linkText="企画ページに移動"
             link={"../project"}
           />
+        </SectionBody>
+
+        <SectionBody>
           <WhatsNewCard
             title="企画ページの特別デザイン作成"
             text="一部ページにおいて特殊なデザインが適用されました!"
             img={"/62nd/whats-new/hauntedHouse.webp"}
             linkText="お化け屋敷ページに移動"
             link={"../project/haunted-house"}
+          />
+          <WhatsNewCard
+            title="おすすめ機能の最適化"
+            text="開催時間やカテゴリ分けに基づいて、より関連した企画がおすすめに表示されるようになりました"
+            // TODO: おすすめ機能が実装されたら画像を追加
+          />
+          <WhatsNewCard
+            title="企画ページのレイアウト変更"
+            text="企画ページのレイアウトを改善し、PC・スマホのどちらでもより見やすく、探している情報に到達しやすくなりました。"
+            linkText="企画ページに移動"
+            link={"../project"}
           />
           <WhatsNewCard
             title="アニメーションの更新"
@@ -89,7 +95,7 @@ export default function Faq() {
       <ContentTitle title="協力" size={1} bigTitle />
       <PageWrapper>
         <SectionBody>
-          <ContentTitle title={"62回 Web"} size={4} />
+          <ContentTitle title={"62回 Web"} size={3} />
           <WhatsNewCard
             title={"村川 卓也"}
             text={
@@ -101,19 +107,24 @@ export default function Faq() {
             }
             link={"https://github.com/MurakawaTakuya"}
             linkText={"GitHub"}
-            style={{ flexDirection: "row" }}
+            underText={"201 commits"}
+            flexButton
           />
           <WhatsNewCard
             title={"鐘ヶ江 航"}
             link={"https://github.com/senntou"}
+            text={"エンジニア協力"}
             linkText={"GitHub"}
-            style={{ flexDirection: "row" }}
+            underText={"71 commits"}
+            flexButton
           />
           <WhatsNewCard
             title={"梅原 ヒロユキ"}
             link={"https://github.com/ViniciusBrJp"}
+            text={"エンジニア協力"}
             linkText={"GitHub"}
-            style={{ flexDirection: "row" }}
+            underText={"13 commits"}
+            flexButton
           />
           <WhatsNewCard
             title={"大原 一将"}
@@ -124,8 +135,13 @@ export default function Faq() {
                 第59・60・61回広報局
               </>
             }
+            underText={"7 commits"}
           />
-          <WhatsNewCard title={"小林 央河"} text={"第60・61回広報局"} />
+          <WhatsNewCard
+            title={"小林 央河"}
+            text={"第60・61回広報局"}
+            underText={"3 commits"}
+          />
         </SectionBody>
         <SectionBody>
           <ContentTitle title={"3Dマップ"} size={3} />
@@ -134,74 +150,9 @@ export default function Faq() {
             text={"第61回企画局・第62回広報局"}
             link={"https://github.com/Shika174"}
             linkText={"GitHub"}
-            style={{ flexDirection: "row" }}
           />
         </SectionBody>
       </PageWrapper>
     </>
-  );
-}
-
-function WhatsNewCard({
-  title = "",
-  text = "",
-  img,
-  link,
-  linkText,
-  style,
-}: {
-  title?: string;
-  text?: string | React.ReactNode;
-  img?: string;
-  link?: string;
-  linkText?: string;
-  style?: React.CSSProperties;
-}) {
-  return (
-    <Animation>
-      <Card
-        sx={{
-          margin: "10px 5px 20px",
-          background:
-            "linear-gradient(150deg, #ffefe2 0%, #ffffff 80%, #ffefe1)",
-          boxShadow: "6px 6px 8px 0px #dcdcdc",
-          border: "thin solid #ffddaf",
-          ...style,
-        }}
-      >
-        <div>
-          <Typography level="title-lg" sx={{ paddingBottom: "3px" }}>
-            {title}
-          </Typography>
-          <Typography level="body-sm">{text}</Typography>
-        </div>
-        {img && <img src={img} srcSet={img} loading="lazy" alt="" />}
-        {linkText && (
-          <CardContent orientation="horizontal">
-            <Button
-              variant="outlined"
-              size="md"
-              color="primary"
-              sx={{
-                ml: "auto",
-                alignSelf: "center",
-                fontWeight: 600,
-                color: "var(--theme-color)",
-                borderColor: "var(--theme-color)",
-                "&:hover": {
-                  backgroundColor: "#ffeded",
-                },
-                transition: ".2s",
-              }}
-              component="a"
-              href={link}
-              target="_blank"
-            >
-              {linkText}
-            </Button>
-          </CardContent>
-        )}
-      </Card>
-    </Animation>
   );
 }
