23dd4e37abde435518f4c7c8f9f984c96f89bd37
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-12 17:38:14 +0900

    AnnounceDataのディレクトリを変更

--- src/components/Annoucements/Annoumcements.tsx
+++ src/components/Annoucements/Annoumcements.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import ContentTitle from "../Content/ContentTitle/ContentTitle";
 import PageWrapper from "../Content/PageWrapper/PageWrapper";
 import styles from "./Announcements.module.scss";
-import { announcements } from "./AnnouncementsData";
+import { announcements } from "../../app/AnnouncementsData";
 import NewsAccordion from "./NewsAccordion/NewsAccordion";
 import { Button } from "@mui/material";
 import { IoMdArrowDropdown } from "react-icons/io";
